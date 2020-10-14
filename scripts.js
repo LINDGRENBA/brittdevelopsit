08998d1d7be1b998670c40b7a6d8d8b63a7cd344
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-14 12:21:31 -0700

    Update projectsArray with src and remaining images.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,30 @@
 let projectsArray = [
   {
-    src: "images/pig-dice.PNG",
-    title: "Pig Dice",
-    description: "A fun game of chance created with object oriented JavaScript and jQuery."
+    src: "images/pet-friendly.PNG",
+    url: "https://github.com/LINDGRENBA/petFriendly",
+    title: "PetFriendly",
+    description: "An application to help take the stress out of traveling with your pets."
   },
   {
-    url: "",
+    src: "images/safer-spaces.PNG",
+    url: "https://github.com/LINDGRENBA/SaferSpacesClient",
     title: "Safer Spaces",
     description: "A place to report incidences of harrasment in the community to help create safer spaces for all."
   },
   {
-    url: "",
-    title: "PetFriendly",
-    description: "An application to help take the stress out of traveling with your pets."
+    src: "images/pig-dice.PNG",
+    url: "https://lindgrenba.github.io/pig-dice/",
+    title: "Pig Dice",
+    description: "A fun game of chance created with object oriented JavaScript and jQuery."
   },
-]
+];
 
 document.addEventListener("DOMContentLoaded", function(event) {
   let myprojects = document.getElementById("projects");
   
   projectsArray.forEach(project => {
     myprojects.innerHTML += 
-    "<div class='project-item'><img class='project-img' src='" + project.src + "' alt=''><h4><a class='project-link'>" + project.title + "</a></h4><p class='project-para'>" + project.description +"</p></div>";
+    "<div class='project-item'><img class='project-img' src='" + project.src + "' alt=''><h4><a href='" + project.url + "' target='_blank' class='project-link'>" + project.title + "</a></h4><p class='project-para'>" + project.description +"</p></div>";
   });
 
 });
